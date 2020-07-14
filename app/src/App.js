173a15d02c754bd6322c5a6bd0cc89cafd770db4
Nicolas Constantinou <nicolasconstantinou@Mac-mini.connect>
2020-07-14 16:14:11 +0100

    frontend setup redux  and react-router

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import store from './store'
+import Login from './screens/Login'
+import Register from './screens/Register'
+import TodoList from './screens/TodoList'
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={TodoList}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/register" exact component={Register}/>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
